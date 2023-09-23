f81f3310c274741e18485591aa0c0768fa626ef1
Hassan Mourtada <hassanmourtada15>
2023-09-23 10:24:54 +0300

    step 3

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
   res.send('Ok!')
 })
 
+app.use('/test', (req, res) => {
+  res.status(200).send({status:200, message:"ok"})
+})
+
+app.use('/time', (req, res) => {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0')
+
+  const currentTime = `${hours}:${seconds}`;
+
+  res.status(200).send({status:200, message: currentTime});
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
