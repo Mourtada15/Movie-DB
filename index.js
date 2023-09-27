19a6ea4312d84f4735b12c39f31467d4d85757f5
Hassan Mourtada <hassanmourtada15>
2023-09-27 19:32:04 +0300

    step 7

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const port = 3000
 app.set('case sensetive routing', false);
 
 const movies = [
-  { title: 'Jaws', year: 1975, rating: 8 },
-  { title: 'Avatar', year: 2009, rating: 7.8 },
-  { title: 'Brazil', year: 1985, rating: 8 },
-  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+  { id: '1', title: 'Jaws', year: 1975, rating: 8 },
+  { id: '2', title: 'Avatar', year: 2009, rating: 7.8 },
+  { id: '3', title: 'Brazil', year: 1985, rating: 8 },
+  { id: '4', title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
 ]
 
 app.get('/', (req, res) => {
@@ -53,6 +53,32 @@ app.use('/create', (req, res) => {
 
 app.get('/movies/read', (req, res) => {
   res.status(200).send({status:200, message: movies})
+})
+
+app.get('/movies/read/by-date', (req, res) => {
+  const moviesByDate = movies.sort((a, b) => a.year - b.year);
+  res.status(200).send({status:200, message: moviesByDate})
+})
+
+app.get('/movies/read/by-rating', (req, res) => {
+  const moviesByRating = movies.sort((a, b) => b.rating - a.rating);
+  res.status(200).send({status:200, message: moviesByRating})
+})
+
+app.get('/movies/read/by-title', (req, res) => {
+  const moviesByTitle = movies.sort((a, b) => a.title.localeCompare(b.title));
+  res.status(200).send({status:200, message: moviesByTitle})
+})
+
+app.get('/movies/read/id/:ID', (req, res) => {
+  const { ID } = req.params; 
+  const movie = movies.find((m) => m.id === ID);
+
+  if (movie) {
+    res.status(200).send({status:200, data: movie})
+  }
+  else
+  res.status(404).send({status:404, error:true, message: `The movie ${ID} does not exist`})
 })
 
 app.use('/update', (req, res) => {
